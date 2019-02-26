77d099f6cdfb59afd421394fe887ec771233144c
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-26 23:18:52 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,7 +4,10 @@ require "pry"
 
 def alphabetize(arr)
   
-  arr.sort_by { |phrase| phrase.split("").first}
+  arr.sort_by do |phrase| 
+    binding.pry
+    phrase.split("").first
+  end
  
   
 end
